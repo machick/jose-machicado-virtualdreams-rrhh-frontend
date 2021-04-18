3084a242558075f7532b911f4d21c9ef834b4101
Jose Machicado <Josemiguelmc_1998@hotmail.com>
2021-04-18 00:04:18 -0300

    caracteres especiales en mensaje

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const expresiones={
     nombre: /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/,        
     email:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
     asunto:/^\w+(\s*\w)+$/,
-    textarea:/^\w+(\s*\w)+$/
+    textarea:/^(\w){2,}\.*/
 }
 
 
